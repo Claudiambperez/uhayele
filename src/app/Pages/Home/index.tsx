2f0e964df3ba4be7c824b7750f701e3fdd8365d5
Claudiambperez <claudiabarbon@yahoo.com>
2025-08-15 20:45:25 +0100

    landing page v5 deploy

--- src/app/Pages/Home/index.tsx
+++ src/app/Pages/Home/index.tsx
@@ -1,14 +1,14 @@
 //SECTION 
 import About from "../About"
 import Classes from "./Classes"
-import Hero from "./Hero"
+
 import Teams from "./Teams"
 import Testimonial from "./Testimonials"
 
 const HomePage = () => {
   return (
     <>
-      <Hero />
+     
        <About />
        <Classes />
        <Teams />
